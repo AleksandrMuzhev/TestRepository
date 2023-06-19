4b1413107223cecd6a91ba6d6a8c583a5e3fb545
Aleksandr <muzhev1990@mail.ru>
2023-06-19 23:40:54 +0500

    Fail test

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,18 @@
+package ru.netology.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void test1() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 0;
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
